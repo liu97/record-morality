17a5d3ebd63a58d56d4a128eabc1b2979ace51a6
liu97 <1139472029@qq.com>
2018-12-27 21:24:17 +0800

    添加mocha

---
+++ test/async-test.js
@@ -0,0 +1,12 @@
+/*
+* @Author: liuchuanfu
+* @Date:   2018-12-27 21:20:24
+* @Last Modified time: 2018-12-27 21:21:56
+* @Email: chuanfuliu@sohu-inc.com
+*/
+const hello = require('./user1');
+
+it('#async function', async () => {
+    let r = await hello();
+    assert.strictEqual(r, 15);
+});
